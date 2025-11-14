512d55bb74fa194aa4ec91c449231b05e618c92b
Riley Barka <barkar@wit.edu>
2025-11-14 12:57:08 -0500

    Updated version of Linear Regression

--- linearRegression.py
+++ linearRegression.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-# load preprocessed dataset 
-df = pd.read_csv("data_with_datetime.csv", index_col="datetime", parse_dates=True)
-
-# Drop missing values (same across both models)
+# load final dataset
+df = pd.read_csv("final_data.csv", index_col = "datetime", parse_dates = True)
 df = df.dropna()
 
-# define fetures and target
+# features and target
 X = df[["Voltage",
         "Global_reactive_power",
         "Sub_metering_1",
@@ -20,53 +19,47 @@ X = df[["Voltage",
 
 y = df["Global_active_power"]
 
-# split data 
+# split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size = 0.2, random_state = 42
 )
 
 print("Train shape:", X_train.shape)
 print("Test shape:", X_test.shape)
 
-# train Linear Regression model
+# scale features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# train LR
 lin_reg = LinearRegression()
-lin_reg.fit(X_train, y_train)
+lin_reg.fit(X_train_scaled, y_train)
 
-# Predict
-y_pred = lin_reg.predict(X_test)
+# predict
+y_pred = lin_reg.predict(X_test_scaled)
 
-# 5. Model evaluation
+# evaluate
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print("\nLinear Regression Results")
+print("\nLinear Regression Results (Scaled)")
 print(f"MAE:  {mae:.4f}")
 print(f"RMSE: {rmse:.4f}")
 print(f"R²:   {r2:.4f}")
 
-# 6. Plot actual vs predicted
-plt.figure(figsize=(8,8))
-plt.scatter(y_test[:5000], y_pred[:5000], alpha=0.3)
-min_val = min(y_test.min(), y_pred.min())
-max_val = max(y_test.max(), y_pred.max())
-plt.plot([min_val, max_val], [min_val, max_val], 'r--')
-plt.xlabel("Actual Global Active Power")
-plt.ylabel("Predicted Global Active Power")
-plt.title("Actual vs Predicted (Linear Regression)")
-plt.grid(True)
-plt.show()
-
-# 7. Plot feature coefficients
+# coefficients
 coef_df = pd.DataFrame({
     "Feature": X.columns,
     "Coefficient": lin_reg.coef_
-}).sort_values(by="Coefficient", ascending=False)
+}).sort_values(by = "Coefficient", ascending = False)
 
-plt.figure(figsize=(6,4))
+plt.figure(figsize = (6,4))
 plt.barh(coef_df["Feature"], coef_df["Coefficient"])
 plt.xlabel("Coefficient Value")
 plt.title("Feature Influence (Linear Regression)")
 plt.tight_layout()
 plt.show()
+
